935e3b05d215d3f76bb7a6acc7bddf5e510f3b13
DjaintyDjust <DjaintyDjust@outlook.com>
2024-06-15 01:26:01 +0200

    Update redirect.js

--- assets/redirect.js
+++ assets/redirect.js
@@ -1,9 +1,8 @@
-// Docs/assets/redirect.js
-
 const redirects = {
     "/nanotech": "https://www.roblox.com/games/11569994474/nanotech-project-NPRF",
     "/nprf": "https://www.roblox.com/games/11569994474/nanotech-project-NPRF",
     "/roblox": "https://www.roblox.com/users/544088422/profile",
+    "/profile": "https://www.roblox.com/users/544088422/profile",
     "/youtube": "https://youtube.com/@DaintyDust",
     "/twitter": "https://twitter.com/NickV535",
     "/x": "https://twitter.com/NickV535",
