a84fe0be0f9e4657fddf8e49f63eb0df33395233
DjaintyDjust <DjaintyDjust@outlook.com>
2024-06-14 12:35:58 +0200

    Create redirect.js

---
+++ Docs/assets/redirect.js
@@ -0,0 +1,19 @@
+// redirect.js
+
+const redirects = {
+    "/nanotech": "https://www.roblox.com/games/11569994474/nanotech-project-NPRF",
+    "/nprf": "https://www.roblox.com/games/11569994474/nanotech-project-NPRF",
+    "/roblox": "https://www.roblox.com/users/544088422/profile",
+    "/youtube": "https://youtube.com/@DaintyDust",
+    "/twitter": "https://twitter.com/NickV535",
+    "/x": "https://twitter.com/NickV535",
+    "/github": "https://github.com/DaintyDust",
+    "/group": "https://www.roblox.com/groups/8193767/Nick-Studios#!/about",
+};
+
+const path = window.location.pathname;
+if (redirects[path]) {
+    window.location.href = redirects[path];
+} else {
+    window.location.href = '/Docs/error.html';
+}
